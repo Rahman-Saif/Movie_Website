a6ba603c0f9896df67a4e85457e903bbc30e9409
Rahman <rahmansaif.413@gmail.com>
2023-07-13 09:53:16 +0600

    initial commit

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./navbar.scss";
+import { ArrowDropDown, Notifications, Search } from '@mui/icons-material';
+
+const Navbar = () => {
+  return (
+    <div className='navBar'>
+      <div className="container">
+        <div className="left">
+        <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1000&h=563&crop=1"/>
+        <span>Homepage</span>
+        <span>Series</span>
+        <span>Movies</span>
+        <span>New and Popular</span>
+        <span>My List</span>
+        </div>
+        <div className="right">
+            <Search className='icon'/>
+            <span>Kid</span>
+            <Notifications className='icon'/>
+            <div className="profile">
+                <ArrowDropDown className='icon'/>
+                <div className="options">
+                    <span>Settings</span>
+                    <span>Logout </span>
+                </div>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Navbar from '../components/Nav/Navbar'
+import "./home.scss";
+const Home = () => {
+  return (
+    <div className="home">
+     <Navbar/>
+    </div>
+  )
+}
+
+export default Home
